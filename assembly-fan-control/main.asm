06df5630db1ae478de0ac5d55b68dcc679113be7
Harrison Bouche <harrison@sm.media>
2022-12-05 12:36:51 -0500

    Set up loop that checks values, untested

--- assembly-fan-control/main.asm
+++ assembly-fan-control/main.asm
@@ -25,16 +25,17 @@ rjmp INT0_vect
 LDI R16, 0xFF ;(all ones)
 out DDRC, R16 ; Port C initialized to output / send info to LEDs
 
-LDI R17, 0x00 ; (all zeroes)
-out DDRA, R17 ; Port A initialized to input / takes in temperature
+; Fucking shit port setting?
+; LDI R17, 0x00 ; (all zeroes)
+;out DDRA, R17 ; Port A initialized to input / takes in temperature
 
-LDI r18, 0x00110010 ; setting fan register to 50
+LDI r18, 0b00110010 ; setting fan register to 50
 
-LDI r19, 0x00011001 ;min fan speed 25%
+LDI r19, 0b00011001 ;min fan speed 25%
 
-LDI r20, 0x01100100 ;max fan speed 100%
+LDI r20, 0b01100100 ;max fan speed 100%
 
-LDI r21, 0x1000110101 ;30 celsius for temp to be at
+LDI r21, 0b1000110101 ;30 celsius for temp to be at
 ;create variable for thermistor value to be
 
 ;
@@ -48,24 +49,25 @@ LDI r21, 0x1000110101 ;30 celsius for temp to be at
 
 ; ADC setup
 initial_ADC:
-	ldi R16, 0x20
-	sts ADMUX, R16
-	ldi R16, 0x97
-	sts ADCSRA, R16
-	clr R16 ; equivalent to ldi R16, 0x00
-	sts ADCSRB, R16
+	ldi R22, 0x20
+	sts ADMUX, R22
+	ldi R22, 0x97
+	sts ADCSRA, R22
+	clr R22 ; equivalent to ldi R16, 0x00
+	sts ADCSRB, R22
 
 ; start ADC conversion
-SC:
-	lds R16, ADCSRA
-	ori R16, 0b01000000 ; enable ADSC to trigger conversion
+StartConversion:
+	lds R22, ADCSRA
+	ori R22, 0b01000000 ; enable ADSC to trigger conversion
 
 ; check conversion status
 CheckEOC:
-	lds R16, ADCSRA
-	SBRS R16, ADIF ; check if bit 7 of register 16 is equivalent to ADIF (bit 5 of ADCSRA), skip if true
+	lds R22, ADCSRA
+	SBRS R22, ADIF ; check if bit 7 of register 16 is equivalent to ADIF (bit 5 of ADCSRA), skip if true
 	RJMP CheckEOC
 	CALL ReadAOC
+	rjmp Main
 
 ; to read retrieved data
 ReadADC:
@@ -85,35 +87,42 @@ ReadADC:
 ; START FAN CONTROL CODE
 ;
 
-main:
+Main:
+	Call StartConversion ; check ADC for updated thermistor voltage value
 	Call TempCheck ;call to check the temp right away may include branches instead of call
+	rjmp Main
+
+Display:
+	out PORTC, r25
+	RET
 
 TempCheck: ;Checks the temperature of the thermistor and then sees if temp is lower or higher than it should be
-	CPI r17, r21
-	BR ;if temp read (r17) is lower than r21 then slow fan else if r17 is higher increase fan if neither keep speed.
+	Call Display ; send numbers to LED displays
+	Call Delay
+	CP r25, r21
+	BRLO FanSpeedDec
+	BRSH FanSpeedInc
+	RET
 
 FanSpeedInc: ;increases fan speed to increase cooling to lower temp
+	cpse r20, r18 ; skips next line if fan speed is at max value
 	add r18, 0x01; add initial speed by 1 bit
-	call delay
 	rjmp TempCheck ;loops back to check fan speed 
 
 FanSpeedDec: ;decreases fan speed to increase temp
+	cpse r19, r18 ; skips next line if fan speed is at min value
 	sub r18, 0x01 ;sub initial speed by 1 bit
-	call delay
 	rjmp TempCheck ;loops back to check fan speed 
 
-TempStable: ;resistor has reached its targeted temp.
-	rjmp main
-
 Delay: ; 2 second delay to avoid constant reading.
-	ldi r20, 163
-	ldi r21, 86
-	ldi r22, 0
-L1: dec r22
+	ldi r29, 163
+	ldi r30, 86
+	ldi r31, 0
+L1: dec r31
 	brne L1
-	dec r21
+	dec r30
 	brne L1
-	dec r20
+	dec r29
 	brne L1
 	ret
 
